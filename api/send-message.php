41e459249d65166513bc9560e8bb2ddd587a095d
Ruslan Gataullin <gataurus@mail.ru>
2026-07-04 00:33:23 +0300

    Create send-message.php

---
+++ api/send-message.php
@@ -0,0 +1,36 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    die(json_encode(['error' => 'Method not allowed']));
+}
+
+// Плейсхолдеры заменяются GitHub Actions
+$MESSAGE_SERVER_URL = '{{MESSAGE_SERVER_URL}}';
+$SECRET_TOKEN = '{{SECRET_TOKEN}}';
+
+$input = json_decode(file_get_contents('php://input'), true);
+$input['auth_token'] = $SECRET_TOKEN;
+
+$ch = curl_init($MESSAGE_SERVER_URL);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($input));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+
+$response = curl_exec($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+http_response_code($http_code);
+echo $response;
